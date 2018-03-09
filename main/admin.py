6bfad13c1140a903e99e05a6a00ea46f6f0b0baa
katalyzator <web.coder96@gmail.com>
2018-03-09 17:17:44 +0600

    added new Model with Migrations without views configuration

--- main/admin.py
+++ main/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Slider
+
+
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+
+    class Meta:
+        model = Slider
+
+
+admin.site.register(Slider, SliderAdmin)
--- main/models.py
+++ main/models.py
@@ -2,4 +2,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import smart_unicode
 
+
+class Slider(models.Model):
+    title = models.CharField(max_length=500, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images/slider', verbose_name='Картинка')
+
+    class Meta:
+        verbose_name_plural = 'Слайдер на главной странице'
+        verbose_name = 'слайд'
+
+    def __unicode__(self):
+        return smart_unicode(self.title)
