6779620807d0723c0671b16cf997b3e540d4f0dc
katalyzator <web.coder96@gmail.com>
2018-03-09 17:20:29 +0600

    add new Migration

--- main/views.py
+++ main/views.py
@@ -3,9 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from main.models import Slider
+
 
 def index_view(request):
-    context = {}
+    sliders = Slider.objects.all()
+    context = {
+        "sliders": sliders
+    }
     template = 'main/index.html'
 
     return render(request, template, context)
